09ab35767e3b3d97a3e1fa72298ae6400b1f5ac0
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-03 04:49:59 -0500

    Project Break Step 6 Incomplete

--- TOP_Rock_Paper_Scissors/script.js
+++ TOP_Rock_Paper_Scissors/script.js
@@ -2,7 +2,54 @@
 // Variables/Functions
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3); 
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber == 0) return 'ROCK';
+  if (randomNumber == 1) return 'PAPER';
+  if (randomNumber == 2) return 'SCISSORS';
 }
-let compChoice = getComputerChoice();
+function getHumanChoice() {
+   let humanChoice = prompt("Please choose Rock, Paper, or Scissors!");
+   return humanChoice.toUpperCase();
+}
 
+
+
+let compChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+console.log(compChoice);
+console.log(humanChoice);
+
+
+// game logic
+function playGame(params) {
+    
+    let humanScore    = 0;
+    let computerScore = 0;
+
+function playRound(humanChoice, compChoice) {
+if (humanChoice === 'ROCK' && compChoice === 'SCISSORS') {
+    console.log(`You WIN! ${humanChoice} beats ${compChoice}.`);
+    humanScore += 1;
+} else if (humanChoice === 'PAPER' && compChoice === 'ROCK') {
+    console.log(`You WIN! ${humanChoice} beats ${compChoice}.`);
+    humanScore += 1;
+} else if (humanChoice === 'SCISSORS' && compChoice === 'PAPER') {
+    console.log(`You WIN! ${humanChoice} beats ${compChoice}.`);
+    humanScore += 1;
+} else if (humanChoice === compChoice) {
+    console.log("Draw! Try again!")
+} else {
+    console.log(`You LOSE! ${compChoice} beats ${humanChoice}!`);
+    computerScore += 1;
+}
+}
+for (let round = 0; round < 5; round++) {
+
+    playRound(humanChoice, compChoice);
+    getComputerChoice();
+    getHumanChoice();
+    }
+}
+
+playGame();

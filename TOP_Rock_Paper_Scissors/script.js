643fee95f267a959658369a2989ee0f84cb34358
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-03 20:16:29 -0500

    Project complete.

--- TOP_Rock_Paper_Scissors/script.js
+++ TOP_Rock_Paper_Scissors/script.js
@@ -3,52 +3,58 @@
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber == 0) return 'ROCK';
-  if (randomNumber == 1) return 'PAPER';
-  if (randomNumber == 2) return 'SCISSORS';
+  if (randomNumber === 0) {
+    randomNumber = "ROCK";
+  } else if (randomNumber === 1) {
+    randomNumber = "PAPER";
+  } else {
+    randomNumber = "SCISSORS";
+  }
+  console.log(`Computer Choice: ${randomNumber}`)
+  return randomNumber;
 }
 function getHumanChoice() {
-   let humanChoice = prompt("Please choose Rock, Paper, or Scissors!");
-   return humanChoice.toUpperCase();
+   let choice = prompt("Please choose Rock, Paper, or Scissors!").toUpperCase();
+   console.log(`Human Choice: ${choice}`);
+   return choice; 
+}
+let humanScore = 0;
+let compScore = 0;
+
+function playRound() {
+    // getComputerChoice();
+    // getHumanChoice();
+    const humanChoice = getHumanChoice();
+    const compChoice = getComputerChoice();
+    if (humanChoice === "ROCK" && compChoice === "SCISSORS") {
+        console.log(`You WIN! ${humanChoice} beats ${compChoice}!`);
+        humanScore++; 
+    } else if (humanChoice === "PAPER" && compChoice === "ROCK") {
+        console.log(`You WIN! ${humanChoice} beats ${compChoice}!`);
+        humanScore++;
+    } else if (humanChoice === "SCISSORS" && compChoice === "PAPER") {
+        console.log(`You WIN! ${humanChoice} beats ${compChoice}!`);
+        humanScore++;
+    } else if (humanChoice === compChoice) {
+        console.log('Tie! No points!')
+    } else {
+        console.log(`You LOSE! ${compChoice} beats ${humanChoice}!`);
+        compScore++;
+    }
+    console.log(`Your score: ${humanScore},  Comp Score: ${compScore}`);
+    console.log('---------------------------------------------------------');
 }
 
-
-
-let compChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-
-console.log(compChoice);
-console.log(humanChoice);
-
-
-// game logic
-function playGame(params) {
-    
-    let humanScore    = 0;
-    let computerScore = 0;
-
-function playRound(humanChoice, compChoice) {
-if (humanChoice === 'ROCK' && compChoice === 'SCISSORS') {
-    console.log(`You WIN! ${humanChoice} beats ${compChoice}.`);
-    humanScore += 1;
-} else if (humanChoice === 'PAPER' && compChoice === 'ROCK') {
-    console.log(`You WIN! ${humanChoice} beats ${compChoice}.`);
-    humanScore += 1;
-} else if (humanChoice === 'SCISSORS' && compChoice === 'PAPER') {
-    console.log(`You WIN! ${humanChoice} beats ${compChoice}.`);
-    humanScore += 1;
-} else if (humanChoice === compChoice) {
-    console.log("Draw! Try again!")
-} else {
-    console.log(`You LOSE! ${compChoice} beats ${humanChoice}!`);
-    computerScore += 1;
-}
-}
-for (let round = 0; round < 5; round++) {
-
-    playRound(humanChoice, compChoice);
-    getComputerChoice();
-    getHumanChoice();
+function playGame() {
+    for (let round = 0; round < 5; round++) {
+        playRound();
+    }
+    if (humanScore > compScore) {
+        console.log("You WIN! You are a lucky Genius!");
+    } else if (compScore > humanScore) {
+        console.log(" You LOSE! Work on your throws!");
+    } else {
+        console.log("Tie Game! Not too shabby!");
     }
 }
 
